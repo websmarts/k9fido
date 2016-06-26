684cfe4b8621474cf542279e85520b931e961a2a
websmarts <iwmaclagan@bigpond.com>
2016-06-26 14:59:16 +1000

    updated welcome message

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -8,7 +8,7 @@
                 <div class="panel-heading">Welcome</div>
 
                 <div class="panel-body">
-                    Your Application's Landing Page.
+                    K9 Project's Landing Page.
                 </div>
             </div>
         </div>
