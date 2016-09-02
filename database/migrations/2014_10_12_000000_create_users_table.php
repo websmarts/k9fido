9c5637473402b7923c9877558fa3e4f5b2b72522
websmarts <iwmaclagan@bigpond.com>
2016-09-02 13:41:11 +1000

    Added roles field into users table

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -18,6 +18,7 @@ class CreateUsersTable extends Migration
             $table->string('email')->unique();
             $table->string('password');
             $table->rememberToken();
+            $table->string('roles')->nullable();
             $table->timestamps();
         });
     }
