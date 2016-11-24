a4e0384ecfcce8df6f86ec277a5297bff4f61941
websmarts <iwmaclagan@bigpond.com>
2016-11-24 11:33:59 +1100

    fixed freight offset zero error if client does not have a postcode

--- app/Services/FreightService.php
+++ app/Services/FreightService.php
@@ -18,9 +18,12 @@ class FreightService
         // echo dumper ($res);
 
         //$local = do_query('select * from freight_localzones where `zone` ="'.$res[0]['zone'].'"');
-        $local = \DB::connection('k9homes')
-            ->select("select * from freight_localzones where `zone` ='{$zones[0]->zone}'");
+        if (isset($zones[0]) && isset($zones[0]->zone)) {
+            $local = \DB::connection('k9homes')
+                ->select("select * from freight_localzones where `zone` ='{$zones[0]->zone}'");
 
-        return [$zones[0]->zone, !empty($local) ? 'local' : 'not local'];
+            return [$zones[0]->zone, !empty($local) ? 'local' : 'not local'];
+        }
+
     }
 }
