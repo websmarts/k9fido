ad383acc756576b70cc3c0407689d5c7518d16ba
websmarts <iwmaclagan@bigpond.com>
2016-11-29 14:04:38 +1100

    product edit form tweak to BoM area

--- resources/views/admin/product/edit.blade.php
+++ resources/views/admin/product/edit.blade.php
@@ -35,7 +35,7 @@
      <div class="row">
         <div class="col-md-6 col-md-offset-1">
             <div class="panel panel-default">
-                <div class="panel-heading">Bill of Material: {{ dump($product->bom)  }}</div>
+                <div class="panel-heading">Bill of Material: {{-- dump($product->bom)  --}}</div>
 
                 <div class="panel-body">
                     <table class="table table-striped">
