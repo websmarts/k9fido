13aaea076f49258e00ba170d4abe6b5b021b05d3
websmarts <iwmaclagan@bigpond.com>
2017-12-10 13:37:46 +1100

    fixed error in list orders if no client attached to order

--- resources/views/admin/lists/orders.blade.php
+++ resources/views/admin/lists/orders.blade.php
@@ -1,3 +1,4 @@
+
 <h3>{{ $title }}</h3>
                     <table class="table table-striped">
                     <thead>
@@ -16,7 +17,8 @@
                             <td>{{ $order->id}}</td>
                             <td>{{ date('j-m-Y',strtotime($order->modified)) }}</td>
 
-                            <td>{{ $order->client->name }}</td>
+                            <td>
+                                {{$order->client ? $order->client->name : 'NO CLIENT ATTACHED TO ORDER'}}</td>
                             <td width="20"><a href="{{ route('order.show', ['id' => $order->id] ) }}"><i class="fa fa-pencil-square-o fa-1x" aria-hidden="true"></i></a></td>
                         </tr>
                         @endforeach
