2ce40487eb6b13a0b88f6ab69a7a1228a7ffb8d8
Ian Maclagan <iwmaclagan@gmail.com>
2021-06-13 18:02:01 +1000

    excel export set to xls to test server

--- app/Http/Controllers/ExportProductsController.php
+++ app/Http/Controllers/ExportProductsController.php
@@ -43,7 +43,7 @@ class ExportProductsController extends Controller
 
         //dd($data);
         
-        $type='csv';
+        $type='xls';
 
         return Excel::create('product_table_export_'.date('dmY_His'), function($excel) use ($data) {
             $excel->sheet('mySheet', function($sheet) use ($data)
