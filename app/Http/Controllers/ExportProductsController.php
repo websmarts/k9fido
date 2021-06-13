10869300dd190eaafa3d591f6cea1bd6c67b43d5
Ian Maclagan <iwmaclagan@gmail.com>
2021-06-13 15:55:05 +1000

    wip

--- app/Http/Controllers/ExportProductsController.php
+++ app/Http/Controllers/ExportProductsController.php
@@ -20,7 +20,7 @@ class ExportProductsController extends Controller
         $rows = DB::connection('k9homes')->table('products')->get();
         // dd($rows);
 
-        // dd(count($rows));
+        dd(count($rows));
 
         $data =[];
         $records = 0;
