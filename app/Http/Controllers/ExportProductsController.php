bab456465f4ec97b0af862d9c713dc837818a774
Ian Maclagan <iwmaclagan@gmail.com>
2021-06-14 22:28:07 +1000

    export products now include ean_barcode

--- app/Http/Controllers/ExportProductsController.php
+++ app/Http/Controllers/ExportProductsController.php
@@ -38,6 +38,11 @@ class ExportProductsController extends Controller
                 }
                 
             }
+            // add checkdigit to barcode to create ean_barcode
+            $data[$records]['ean_barcode'] = '';
+            if(!empty($row->barcode)){
+                $data[$records]['ean_barcode'] = $row->barcode.$this->checkdigit($row->barcode);
+            }
 
         }
 
@@ -52,4 +57,30 @@ class ExportProductsController extends Controller
             });
         })->download($type);
     }
+    function checkdigit( $digits)
+	{
+		if(!$digits){
+			return '';
+		}	
+		
+		$digits = (string) $digits;
+		
+		if(strlen($digits) != 12){
+			return '';
+		}
+		
+		// 1. Add the values of the digits in the even-numbered positions: 2, 4, 6, etc.
+		$even_sum = $digits[1] + $digits[3] + $digits[5] + $digits[7] + $digits[9] + $digits[11];
+		// 2. Multiply this result by 3.
+		$even_sum_three = $even_sum * 3;
+		// 3. Add the values of the digits in the odd-numbered positions: 1, 3, 5, etc.
+		$odd_sum = $digits[0] + $digits[2] + $digits[4] + $digits[6] + $digits[8] + $digits[10];
+		// 4. Sum the results of steps 2 and 3.
+		$total_sum = $even_sum_three + $odd_sum;
+		// 5. The check character is the smallest number which, when added to the result in step 4,  produces a multiple of 10.
+		$next_ten = (ceil($total_sum / 10)) * 10;
+		$check_digit = $next_ten - $total_sum;
+
+		return $check_digit;
+	}
 }
